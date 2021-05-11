06f21437eba9e4e4d8205341b67570f6262e9c2b
Mikhail Macnev <misha@macnev.ru>
2021-05-12 02:36:38 +0500

    Interception: An Effective, Modular and Composable Way of Injecting Behavior and Side-effects in the App Composition

--- EssentialApp/EssentialAppTests/Helpers/EssentialAppTests+Helpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/EssentialAppTests+Helpers.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import EssentialFeed
 
 extension XCTestCase {
     func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
@@ -13,8 +14,13 @@ extension XCTestCase {
             XCTAssertNil(instance, "instance should be nil. Memory leak!", file: file, line: line)
         }
     }
-    
-    func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 1)
-    }
+}
+
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 1)
+}
+
+func uniqueFeed() -> [FeedImage] {
+    [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
 }
---
+++ EssentialApp/EssentialAppTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,14 @@
+
+import EssentialFeed
+
+class FeedLoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+       self.result = result
+   }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> ()) {
+        completion(result)
+    }
+}
