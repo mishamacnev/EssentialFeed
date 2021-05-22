318fd697fb83f8e9865f204c845bb9af6541b212
Mikhail Macnev <misha@macnev.ru>
2021-05-22 23:35:29 +0500

    Replace RemoteImageCommentsLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeed/ImageCommentsAPI/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/ImageCommentsAPI/RemoteImageCommentsLoader.swift
@@ -8,40 +8,10 @@
 import Foundation
 import EssentialFeed
 
-public final class RemoteImageCommentsLoader {
-    private let client: HTTPClient
-    private let url: URL
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-    
-    public init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-    
-    public func load(completion: @escaping (Result) -> ()) {
-        client.get(from: url, completion: { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case .failure(_):
-                completion(.failure(Error.connectivity))
-            case let .success((data, response)):
-                completion(RemoteImageCommentsLoader.map(data, from: response))
-            }
-        })
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try ImageCommentsMapper.map(data, response: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentsLoader {
+    convenience init(client: HTTPClient, url: URL) {
+        self.init(client: client, url: url, mapper: ImageCommentsMapper.map)
     }
 }
--- EssentialFeed/EssentialFeedAPITests/ImageCommentsAPI/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedAPITests/ImageCommentsAPI/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -4,45 +4,6 @@ import EssentialFeed
 import EssentialFeedAPI
 
 class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
-    func test_HHTPClientInvokeWithUrl() {
-        let client = HTTPClientSpy()
-        client.get(from: URL(string: "https://url.com")!, completion: { _ in })
-        XCTAssertFalse(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromUrl() {
-        let url = URL(string: "https://url-3.com")!
-        let (sut, client) = makeSUT(url: url)
-        sut.load { _ in
-            
-        }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_load_requestsDataTwiceFromUrl() {
-        let url = URL(string: "https://url-3.com")!
-        let (sut, client) = makeSUT(url: url)
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_init_doesNotRequestsDataFromUrl() {
-        let (_, client) = makeSUT(url: URL(string: "https://url-2.com")!)
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestFail() {
-        let (sut, client) = makeSUT()
-        let clentError = NSError(domain: "Test", code: 0)
-        
-        expect(sut, toCompleteWith: failure(RemoteImageCommentsLoader.Error.connectivity)) {
-            client.complete(with: clentError, at: 0)
-        }
-    }
     
     func test_load_requestNon2xxResponseFail() {
         let (sut, client) = makeSUT()
@@ -50,7 +11,7 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         let samples = [199, 150, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: failure(RemoteImageCommentsLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             }
@@ -63,7 +24,7 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         let samples = [200, 201, 202, 250, 299]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: failure(RemoteImageCommentsLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 let invalidJSON = Data("invalid json".utf8)
                 client.complete(withStatusCode: code, data: invalidJSON, at: index)
             }
@@ -111,24 +72,6 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_doesNotDeliverResultsAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "https://any.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentsLoader? = RemoteImageCommentsLoader(client: client, url: url)
-        
-        var capturedResults: [RemoteImageCommentsLoader.Result] = []
-        sut?.load { result in
-            capturedResults.append(result)
-        }
-        
-        sut = nil
-        
-        let json = makeItemsJSON([])
-        client.complete(withStatusCode: 200, data: json)
-        
-        XCTAssertTrue(capturedResults.isEmpty)
-    }
-    
     // MARK: - Helpers
     
     private func makeItem(id: UUID, message: String, createdAt: (date: Date, iso8601String: String), username: String) -> (model: ImageComment, json: [String: Any] ) {
@@ -170,7 +113,7 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
             switch (receivedResult, expectedResult) {
             case let (.success(receivedItems), .success(expectedItems)):
                 XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-            case let (.failure(receivedError as RemoteImageCommentsLoader.Error), .failure(expectedError as RemoteImageCommentsLoader.Error)):
+            case let (.failure(receivedError), .failure(expectedError)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
             default:
                 XCTFail("Expected result \(expectedResult) got \(receivedResult) instead",file: file, line: line)
