04ded79d58a8989f54c3fbcedc9eae151ba9c82e
Mikhail Macnev <misha@macnev.ru>
2021-05-26 17:16:23 +0500

    Add image comments title

---
+++ EssentialFeed/EssentialFeed/ImageCommentsPresentation/ImageCommentsPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCommentsPresenter.swift
+//  EssentialFeed
+//
+//  Created by Mikhail Macnev on 26.05.2021.
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString(
+            "IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view")
+    }
+}
