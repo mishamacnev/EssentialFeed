ff05fc04be07dac8192fa344f08ef630801c92bf
Mikhail Macnev <misha@macnev.ru>
2021-06-05 19:26:26 +0500

    Render comments

--- EssentialFeed/EssentialFeediOS/ImageCommentsUI/Views/ImageCommentCell.swift
+++ EssentialFeed/EssentialFeediOS/ImageCommentsUI/Views/ImageCommentCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ImageCommentCell: UITableViewCell {
+public class ImageCommentCell: UITableViewCell {
     @IBOutlet private(set) public var messageLabel: UILabel!
     @IBOutlet private(set) public var usernameLabel: UILabel!
     @IBOutlet private(set) public var dateLabel: UILabel!
