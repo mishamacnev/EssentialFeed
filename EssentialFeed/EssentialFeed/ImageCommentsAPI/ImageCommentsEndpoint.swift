37bfe18a6cabfb128e997d5c057fcdfd78a6d011
Mikhail Macnev <misha@macnev.ru>
2021-06-06 00:53:31 +0500

    Extract endpoint URL creation to the API modules

---
+++ EssentialFeed/EssentialFeed/ImageCommentsAPI/ImageCommentsEndpoint.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+public enum ImageCommentsEndpoint {
+    case get(UUID)
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(id):
+            return baseURL.appendingPathComponent("/v1/image/\(id)/comments")
+        }
+    }
+}
---
+++ EssentialFeed/EssentialFeedAPI/FeedEndpoint.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+public enum FeedEndpoint {
+    case get
+
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            return baseURL.appendingPathComponent("/v1/feed")
+        }
+    }
+}
